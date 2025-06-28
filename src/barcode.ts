fa60225e0508d9b4234acb2a2cb16e492dee6263
SeijiKitamura <seiji.kitamura@gmail.com>
2025-06-28 21:43:08 +0900

    TypeScriptにした

--- src/barcode.ts
+++ src/barcode.ts
@@ -1,8 +1,7 @@
 import { BarcodeDetector } from "barcode-detector/ponyfill";
-globalThis.BarcodeDetector ??= BarcodeDetector;
 
 // 動画開始
-export async function videoStart(element, deviceId = "") {
+export async function videoStart(element: HTMLVideoElement , deviceId: string = ""): Promise<void> {
   const setting = { audio: false,
                     video: { width: 1280,
                              height: 720,
@@ -13,11 +12,10 @@ export async function videoStart(element, deviceId = "") {
                         .then((mediaStream) => {
                           element.srcObject = mediaStream
                         });
-
 }
 
 // 端末に接続されているカメラを取得
-export async function getCameras() {
+export async function getCameras(): Promise<string[][]> {
   return navigator.mediaDevices
                   .enumerateDevices()
                   .then((devices) => devices.filter((device) => device.kind === "videoinput")
@@ -25,7 +23,7 @@ export async function getCameras() {
 }
 
 // select boxを作成
-export async function createCameraBox(element = "") {
+export async function createCameraBox(element: HTMLElement): Promise<void> {
   const cameras = await getCameras();
   cameras.forEach((camera) => {
     const option = document.createElement("option");
@@ -36,29 +34,36 @@ export async function createCameraBox(element = "") {
 }
 
 // バーコードスキャン
-export async function scanBarcode(element, formats, interval, resultEl){
+// 戻り値：バーコードを発見するとresult[0]に値が入る
+export async function scanBarcode(element: HTMLVideoElement, formats: any): Promise<any>{
   element.style.display = "block"
-  console.log("scan start(element: "+ element.id +", formats:" + formats.join(",") + " interval: " + interval + " resultEl: " + resultEl.id +")")
+  console.log("scan start(element: "+ element.id +", formats:" + formats.join(",") + ")")
   try{
     const barcodeDetector = new BarcodeDetector({ formats: formats });
     const results = await barcodeDetector.detect(element)
     if (Array.isArray(results)) {
       if (results.length == 0) { throw new Error("未検出") } 
       else { 
-        element.style.display = "none";
-        showResult(resultEl, results[0])
-        return
+        return results;
       }
     }   
   }
   catch(error){
+    // ページ表示直後にカメラが間に合わずエラーになるのでここで握りつぶす
     //console.log(error)
-    setTimeout(() => scanBarcode(element, formats, interval, resultEl), interval);
+    //setTimeout(() => scanBarcode(element, formats, interval, resultEl), interval);
   }
 }
 
 // 結果表示
-export async function showResult(element, result =[]) {
+export async function showResult(element: HTMLElement, result: any): Promise<void> {
   // elementがinputの場合はinnerTextをvalueに変更する
   element.innerText = result["rawValue"]
+}
+
+// video要素とselect要素の表示・非表示
+export async function toggleElements(clsname: string, swith: string): Promise<void> {
+  document.querySelectorAll(clsname).forEach((element: any) => {
+    element.style.display = swith;
+  })
 }
