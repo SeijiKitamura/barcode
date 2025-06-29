d8c5c42649a7caa8764ace74ca5a983dddffe7ac
SeijiKitamura <seiji.kitamura@gmail.com>
2025-06-29 17:14:00 +0900

    キャンセルボタンを手直し

--- src/barcode.ts
+++ src/barcode.ts
@@ -1,6 +1,8 @@
 import { BarcodeDetector } from "barcode-detector/ponyfill";
 
 let stream: MediaStream;
+let flg: boolean = true;
+
 // 動画開始
 export async function videoStart(
   element: HTMLVideoElement,
@@ -19,23 +21,14 @@ export async function videoStart(
     stream = mediaStream;
     element.srcObject = mediaStream;
   });
+  flg = true;
 }
 
 // 動画停止
 export async function videoStop(){
   if(stream) {
     stream.getTracks().forEach((truck) => { truck.stop() });
-  }
-}
-
-// 動画状態
-export function videoStatus(){
-  try {
-    if(stream && stream["active"] && stream["active"] == true) {
-      return true
-    }else { false }
-  } catch(error){
-    return false
+    flg = false;
   }
 }
 
@@ -51,40 +44,47 @@ export async function getCameras(): Promise<string[][]> {
 }
 
 // select boxを作成
-export async function createCameraBox(element: HTMLElement): Promise<void> {
+export async function createCameraBox(element: HTMLSelectElement): Promise<void> {
   const cameras = await getCameras();
-  await cameras.forEach((camera) => {
-          const option = document.createElement("option");
-          option.value = camera[0];
-          option.textContent = camera[1];
-          element.appendChild(option);
-        });
+  if(element.options.length == 0){
+    await cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera[0];
+            option.textContent = camera[1];
+            element.appendChild(option);
+          });
+  }
 }
 
 // バーコードスキャン
 // 戻り値：バーコードを発見するとresult[0]に値が入る
 export async function scanBarcode(
   element: HTMLVideoElement,
-  formats: any
+  formats: any,
+  interval: number,
+  resultEl: HTMLElement
 ): Promise<any> {
   console.log(
     "scan start(element: " + element.id + ", formats:" + formats.join(",") + ")"
   );
   try {
+    if (!flg) { return  }
     const barcodeDetector = new BarcodeDetector({ formats: formats });
     const results = await barcodeDetector.detect(element);
     if (Array.isArray(results)) {
       if (results.length == 0) {
         throw new Error("未検出");
       } else {
-        return results;
+        showResult(resultEl,results[0])
+        toggleElements(".hide-element", "none");
+        return 
       }
     }
   } 
   catch (error) {
     // ページ表示直後にカメラが間に合わずエラーになるのでここで握りつぶす
     //console.log(error)
-    //setTimeout(() => scanBarcode(element, formats, interval, resultEl), interval);
+    setTimeout(() => scanBarcode(element, formats, interval, resultEl), interval);
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import {
   videoStart,
   videoStop,
-  videoStatus,
   createCameraBox,
   scanBarcode,
-  showResult,
   toggleElements,
 } from "./barcode";
 
 const videoCaptureEl = document.getElementById(
   "video-capture"
 ) as HTMLVideoElement;
-const camerasEl = document.getElementById("cameras")!;
+const camerasEl = document.getElementById("cameras") as HTMLSelectElement;
 const scanBtn = document.getElementById("scan")!;
 const cancelBtn = document.getElementById("cancel")!;
 const resultEl = document.getElementById("result")!;
@@ -22,25 +20,25 @@ const intervalTime: number = 500;
 (async function () {
   // 初期値で撮影開始
   // (画面描写時にこれを実行しないとカメラリストが取得できない)
-  videoStart(videoCaptureEl, "");
+  //videoStart(videoCaptureEl, "");
 
   toggleElements(".hide-element", "none");
   // カメラセット
-  createCameraBox(camerasEl);
+  //createCameraBox(camerasEl);
 
   // カメライベントセット
   camerasEl.addEventListener("change", (e: Event) => {
     const target = e.target as HTMLSelectElement;
     videoStart(videoCaptureEl, target.value);
-    loop_scan(videoCaptureEl, barcodeFormats, intervalTime, resultEl);
   });
 
   // scanボタン投下
   scanBtn.addEventListener("click", () => {
-    resultEl.innerText = "";
     toggleElements(".hide-element", "block");
+    videoStart(videoCaptureEl, "");
+    createCameraBox(camerasEl);
     videoStart(videoCaptureEl, camerasEl.value);
-    loop_scan(videoCaptureEl, barcodeFormats, intervalTime, resultEl);
+    scanBarcode(videoCaptureEl, barcodeFormats, intervalTime, resultEl)
   });
 
   // キャンセルボタン
@@ -48,28 +46,4 @@ const intervalTime: number = 500;
     videoStop();
     toggleElements(".hide-element", "none");
   });
-
-  // 未検出だった場合ループする。
-  async function loop_scan(
-    element: HTMLVideoElement,
-    formats: any,
-    intarval: number,
-    resultElement: HTMLElement
-  ) {
-    if(videoStatus() == undefined ){ return };
-    const barcode = await scanBarcode(element, formats);
-    if (
-      !Array.isArray(barcode) ||
-      (Array.isArray(barcode) && barcode.length == 0)
-    ) {
-      setTimeout(
-        () => loop_scan(videoCaptureEl, barcodeFormats, intarval, resultEl),
-        intarval
-      );
-    } else {
-      showResult(resultElement, barcode[0]);
-      toggleElements(".hide-element", "none");
-      return;
-    }
-  }
 })();
